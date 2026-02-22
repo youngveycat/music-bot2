b82c7e23d7d96461876e5894017af3f9487fa803
youngveycat <gatinhofofo315@gmail.com>
2026-02-22 12:01:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Bot ligado como ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong 🏓');
+  }
+});
+
+client.login(process.env.TOKEN);
